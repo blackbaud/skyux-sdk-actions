70a250893a18aa6f4d288d8eb12c4ab58ae8714d
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-04-20 16:19:57 -0400

    Unit tests

--- src/main.ts
+++ src/main.ts
@@ -58,7 +58,7 @@ async function installCerts(): Promise<void> {
   try {
     await runSkyUxCommand('certs', ['install']);
   } catch (err) {
-    console.log('[SKY UX ERROR]:', err);
+    console.error('[SKY UX ERROR]:', err);
     core.setFailed('SSL certificates installation failed.');
     process.exit(1);
   }
@@ -77,7 +77,7 @@ async function install(): Promise<void> {
 
     await spawn('npm', ['install', '--no-save', '--no-package-lock', 'blackbaud/skyux-sdk-builder-config']);
   } catch (err) {
-    console.log('[SKY UX ERROR]:', err);
+    console.error('[SKY UX ERROR]:', err);
     core.setFailed('Packages installation failed.');
     process.exit(1);
   }
@@ -88,7 +88,7 @@ async function build() {
     await runLifecycleHook('hook-before-script');
     await runSkyUxCommand('build');
   } catch (err) {
-    console.log('[SKY UX ERROR]:', err);
+    console.error('[SKY UX ERROR]:', err);
     core.setFailed('Build failed.');
     process.exit(1);
   }
@@ -100,7 +100,7 @@ async function coverage(configKey: SkyUxCIPlatformConfig) {
     await runLifecycleHook('hook-before-script');
     await runSkyUxCommand('test', ['--coverage', 'library'], configKey);
   } catch (err) {
-    console.log('[SKY UX ERROR]:', err);
+    console.error('[SKY UX ERROR]:', err);
     core.setFailed('Code coverage failed.');
     process.exit(1);
   }
@@ -119,7 +119,7 @@ async function visual(configKey: SkyUxCIPlatformConfig) {
     if (isPullRequest()) {
       await checkNewFailureScreenshots(BUILD_ID);
     }
-    console.log('[SKY UX ERROR]:', err);
+    console.error('[SKY UX ERROR]:', err);
     core.setFailed('End-to-end tests failed.');
     process.exit(1);
   }
@@ -130,7 +130,7 @@ async function buildLibrary() {
     await runSkyUxCommand('build-public-library');
     await runLifecycleHook('hook-after-build-public-library-success');
   } catch (err) {
-    console.log('[SKY UX ERROR]:', err);
+    console.error('[SKY UX ERROR]:', err);
     core.setFailed('Library build failed.');
     process.exit(1);
   }
@@ -148,7 +148,7 @@ async function checkCodeFormat() {
     try {
       await runSkyUxCommand('format-check');
     } catch (err) {
-      console.log('[SKY UX ERROR]:', err);
+      console.error('[SKY UX ERROR]:', err);
       core.setFailed('Library source code is not formatted correctly. Did you run `skyux format`?');
       process.exit(1);
     }
--- src/run-skyux-command.spec.ts
+++ src/run-skyux-command.spec.ts
@@ -18,9 +18,8 @@ describe('Run SKY UX command', () => {
 
   it('should run a SKY UX CLI command', async () => {
     await runSkyUxCommand('test');
-    expect(spawnSpy).toHaveBeenCalledWith('npx', [
-      '-p', '@skyux-sdk/cli',
-      'skyux', 'test',
+    expect(spawnSpy).toHaveBeenCalledWith('skyux', [
+      'test',
       '--logFormat', 'none',
       '--platform', 'gh-actions'
     ]);
@@ -28,9 +27,8 @@ describe('Run SKY UX command', () => {
 
   it('should allow passing arguments', async () => {
     await runSkyUxCommand('test', ['--my-arg', 'foobar']);
-    expect(spawnSpy).toHaveBeenCalledWith('npx', [
-      '-p', '@skyux-sdk/cli',
-      'skyux', 'test',
+    expect(spawnSpy).toHaveBeenCalledWith('skyux', [
+      'test',
       '--logFormat', 'none',
       '--my-arg', 'foobar',
       '--platform', 'gh-actions'
@@ -39,9 +37,8 @@ describe('Run SKY UX command', () => {
 
   it('should allow unsetting the `platform` argument', async () => {
     await runSkyUxCommand('test', [], SkyUxCIPlatformConfig.None);
-    expect(spawnSpy).toHaveBeenCalledWith('npx', [
-      '-p', '@skyux-sdk/cli',
-      'skyux', 'test',
+    expect(spawnSpy).toHaveBeenCalledWith('skyux', [
+      'test',
       '--logFormat', 'none',
       '--headless'
     ]);
