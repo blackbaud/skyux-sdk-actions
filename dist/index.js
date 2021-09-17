1ae61e31c85fba9fa2a311b41e0eb79ca28292b4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-09-17 09:58:35 -0400

    Simplify

--- dist/index.js
+++ dist/index.js
@@ -8660,26 +8660,33 @@ const core = __importStar(__webpack_require__(470));
 const fs = __importStar(__webpack_require__(226));
 const path = __importStar(__webpack_require__(622));
 function validateDependencies(projectName) {
+    core.info('Validationg dependencies...');
     const basePath = path.join(process.cwd(), core.getInput('working-directory'));
     const workspacePackageJsonPath = path.join(basePath, 'package.json');
     const projectPackageJsonPath = path.join(basePath, `projects/${projectName}/package.json`);
     const workspacePackageJson = fs.readJsonSync(workspacePackageJsonPath);
     const projectPackageJson = fs.readJsonSync(projectPackageJsonPath);
-    const result = {};
+    const errors = [];
     // Validate peer dependencies.
     if (projectPackageJson.peerDependencies) {
         for (const packageName in projectPackageJson.peerDependencies) {
             const specificVersion = packageName.replace(/^(\^|~)/, '');
             if (specificVersion !== workspacePackageJson.dependencies[packageName]) {
-                result.error =
-                    `The version of the peer dependency "${packageName}" listed in '${projectPackageJsonPath.replace(basePath, '')}' ` +
-                        `does not match the version of the same dependency listed in '${workspacePackageJsonPath.replace(basePath, '')}'. ` +
-                        `The version provided in the \`dependencies\` section must be specific, and not include a range character ` +
-                        `(for example, write \`"5.1.2"\` instead of \`"^5.1.0"\`).`;
+                errors.push(`The version of the peer dependency "${packageName}" listed in '${projectPackageJsonPath.replace(basePath, '')}' ` +
+                    `does not match the version of the same dependency listed in '${workspacePackageJsonPath.replace(basePath, '')}'. ` +
+                    `The version provided in the \`dependencies\` section must be specific, and not include a range character ` +
+                    `(for example, write \`"5.1.2"\` instead of \`"^5.1.0"\`).`);
             }
         }
     }
-    return result;
+    if (errors.length > 0) {
+        errors.forEach((error) => {
+            core.error(error);
+        });
+        core.setFailed('Errors found with library dependencies.');
+        process.exit(1);
+    }
+    core.info(`Done validating dependencies.`);
 }
 exports.validateDependencies = validateDependencies;
 
--- src/angular-cli/validate-dependencies.ts
+++ src/angular-cli/validate-dependencies.ts
@@ -3,13 +3,9 @@ import * as core from '@actions/core';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
-interface ValidateDependenciesResult {
-  error?: string | null;
-}
+export function validateDependencies(projectName: string): void {
+  core.info('Validationg dependencies...');
 
-export function validateDependencies(
-  projectName: string
-): ValidateDependenciesResult {
   const basePath = path.join(process.cwd(), core.getInput('working-directory'));
 
   const workspacePackageJsonPath = path.join(basePath, 'package.json');
@@ -22,27 +18,36 @@ export function validateDependencies(
   const workspacePackageJson = fs.readJsonSync(workspacePackageJsonPath);
   const projectPackageJson = fs.readJsonSync(projectPackageJsonPath);
 
-  const result: ValidateDependenciesResult = {};
+  const errors: string[] = [];
 
   // Validate peer dependencies.
   if (projectPackageJson.peerDependencies) {
     for (const packageName in projectPackageJson.peerDependencies) {
       const specificVersion = packageName.replace(/^(\^|~)/, '');
       if (specificVersion !== workspacePackageJson.dependencies[packageName]) {
-        result.error =
+        errors.push(
           `The version of the peer dependency "${packageName}" listed in '${projectPackageJsonPath.replace(
             basePath,
             ''
           )}' ` +
-          `does not match the version of the same dependency listed in '${workspacePackageJsonPath.replace(
-            basePath,
-            ''
-          )}'. ` +
-          `The version provided in the \`dependencies\` section must be specific, and not include a range character ` +
-          `(for example, write \`"5.1.2"\` instead of \`"^5.1.0"\`).`;
+            `does not match the version of the same dependency listed in '${workspacePackageJsonPath.replace(
+              basePath,
+              ''
+            )}'. ` +
+            `The version provided in the \`dependencies\` section must be specific, and not include a range character ` +
+            `(for example, write \`"5.1.2"\` instead of \`"^5.1.0"\`).`
+        );
       }
     }
   }
 
-  return result;
+  if (errors.length > 0) {
+    errors.forEach((error) => {
+      core.error(error);
+    });
+    core.setFailed('Errors found with library dependencies.');
+    process.exit(1);
+  }
+
+  core.info(`Done validating dependencies.`);
 }
