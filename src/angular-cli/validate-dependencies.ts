76e4768743e29ee24d8473e5bcd8e90db2751a0b
Steve Brush <steve.brush@blackbaud.com>
2021-09-21 09:24:36 -0400

    Fix dependency validation to handle complex ranges (#36)

--- src/angular-cli/validate-dependencies.ts
+++ src/angular-cli/validate-dependencies.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core';
 
 import * as fs from 'fs-extra';
 import * as path from 'path';
+import semver from 'semver';
 
 function validateDependencySection(
   section: string,
@@ -12,26 +13,36 @@ function validateDependencySection(
   const errors: string[] = [];
 
   for (const packageName in projectPackageJson[section]) {
-    const peerVersion = projectPackageJson[section][packageName];
-    const specificPeerVersion = peerVersion.replace(/^(\^|~)/, '');
+    const targetVersion = projectPackageJson[section][packageName];
+
+    const minTargetVersion = semver.minVersion(targetVersion)!.version;
+
     const workspaceVersion = workspacePackageJson.dependencies[packageName];
 
     if (!workspaceVersion) {
       errors.push(
-        `The package "${packageName}" listed in the \`${section}\` section of 'projects/${projectName}/package.json' was not found in the root 'package.json' \`dependencies\` section. Install the package at the root level and try again.`
+        `The package "${packageName}" listed in the \`${section}\` section of 'projects/${projectName}/package.json' ` +
+          `was not found in the root 'package.json' \`dependencies\` section. Install the package at the root level and try again.`
       );
       continue;
     }
 
-    const workspaceSpecificVersion = workspaceVersion.replace(/^(\^|~)/, '');
+    const minWorkspaceVersion = semver.minVersion(workspaceVersion)!.version;
 
-    if (workspaceVersion !== workspaceSpecificVersion) {
+    if (workspaceVersion !== minWorkspaceVersion) {
       errors.push(
-        `The version listed in 'package.json' for "${packageName}@${workspaceVersion}" must be set to a specific version (without a semver range character), and set to the minimum version satisfied by the range defined in the \`${section}\` section of 'projects/${projectName}/package.json' (wanted "${packageName}@${peerVersion}"). To address this problem, set "${packageName}" to (${specificPeerVersion}) in the root 'package.json'.`
+        `The version listed in 'package.json' for "${packageName}@${workspaceVersion}" must be set to a specific version ` +
+          `(without a semver range character), and set to the minimum version satisfied by the range defined in the \`${section}\` ` +
+          `section of 'projects/${projectName}/package.json' (wanted "${packageName}@${targetVersion}"). To address this problem, set ` +
+          `"${packageName}" to (${minTargetVersion}) in the root 'package.json'.`
       );
-    } else if (specificPeerVersion !== workspaceVersion) {
+    } else if (workspaceVersion !== minTargetVersion) {
       errors.push(
-        `The version (${workspaceVersion}) of the package "${packageName}" in the \`dependencies\` section of 'package.json' does not meet the minimum version requirements of the range defined in the \`${section}\` section of 'projects/${projectName}/package.json' (wanted "${packageName}@${peerVersion}"). Either increase the minimum supported version in 'projects/${projectName}/package.json' to (^${workspaceSpecificVersion}), or downgrade the version installed in the root 'package.json' to (${specificPeerVersion}).`
+        `The version (${workspaceVersion}) of the package "${packageName}" in the \`dependencies\` section of 'package.json' ` +
+          `does not meet the minimum version requirements of the range defined in the \`${section}\` section of ` +
+          `'projects/${projectName}/package.json' (wanted "${packageName}@${targetVersion}"). Either increase the minimum ` +
+          `supported version in 'projects/${projectName}/package.json' to (^${minWorkspaceVersion}), or downgrade the ` +
+          `version installed in the root 'package.json' to (${minTargetVersion}).`
       );
     }
   }
@@ -85,13 +96,14 @@ export function validateDependencies(projectName: string): void {
     }
 
     if (errors.length > 0) {
+      console.log('ERRORS:', errors, errors.length);
       errors.forEach((error) => {
         core.error(error);
       });
       throw new Error('Errors found with library dependencies.');
     }
 
-    core.info(`Done validating dependencies.`);
+    core.info(`Done validating dependencies. OK.`);
   } catch (err) {
     core.setFailed('Failed to validate library dependencies.');
     console.error(err);
