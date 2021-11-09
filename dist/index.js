6e54e387243e10404438042647304cebf5a74fca
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-11-09 16:39:57 -0500

    Just run visual, for now

--- dist/index.js
+++ dist/index.js
@@ -26820,7 +26820,7 @@ function executeAngularCliSteps(buildId) {
             yield tag_skyux_packages_1.tagSkyuxPackages(packageMetadata);
         }
         else {
-            yield coverage(buildId, projectName);
+            // await coverage(buildId, projectName);
             yield visual(buildId, `${projectName}-showcase`, angularJson);
         }
     });
