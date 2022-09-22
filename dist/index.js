6bec0a5e647d078d0b613e83ade22cd6e7fa1275
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-09-22 13:28:06 -0400

    fix

--- dist/index.js
+++ dist/index.js
@@ -26025,7 +26025,7 @@ async function executeAngularCliSteps(buildId) {
         (0, validate_dependencies_1.validateDependencies)(projectName);
     }
     // Needed to get WebkitHeadless working.
-    await (0, spawn_1.spawn)('sudo', ['apt-get', 'install', 'opus-tools', 'libharfbuzz']);
+    await (0, spawn_1.spawn)('sudo', ['apt-get', 'install', 'opus-tools', 'harvbuzz-icu']);
     await install();
     await (0, run_lifecycle_hook_1.runLifecycleHook)('hook-before-script');
     await coverage(buildId, projectName);
